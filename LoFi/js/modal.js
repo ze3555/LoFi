127ddfd028745dcdfbf73ed015292548dc529192
ze3555 <mmikheev58@gmail.com>
2025-08-13 07:33:10 +0700

    Delete LoFi directory

--- LoFi/js/modal.js
+++
@@ -1,9 +0,0 @@
-// js/modal.js
-export function showModal(modal) {
-  modal.classList.remove("hidden");
-  document.body.style.overflow = "hidden";
-}
-export function hideModal(modal) {
-  modal.classList.add("hidden");
-  document.body.style.overflow = "";
-}
